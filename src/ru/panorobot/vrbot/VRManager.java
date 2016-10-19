963bcfd419076d40be84c59e5ff749852508668f
skysamara <skysamara@gmail.com>
2016-10-19 17:50:26 +0300

    Изменено имя файла фотки

--- src/ru/panorobot/vrbot/VRManager.java
+++ src/ru/panorobot/vrbot/VRManager.java
@@ -6,6 +6,8 @@ import android.view.SurfaceHolder;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 1. Открывает главное окно с кнопкой Старнт
@@ -54,7 +56,13 @@ public class VRManager implements Camera.PictureCallback{
                 // TODO: 22.09.2016 Warning:(197, 25) Result of 'File.mkdirs()' is ignored
             }
 
-            FileOutputStream os = new FileOutputStream(String.format("/sdcard/vrbot/%d.jpg", System.currentTimeMillis()));
+//            FileOutputStream os = new FileOutputStream(String.format("/sdcard/vrbot/%d.jpg", System.currentTimeMillis()));
+            String fileName = new SimpleDateFormat("yyyy.MM.dd_HH-mm-ss").format(new Date());
+            fileName = fileName + ".jpg";
+            fileName = "/sdcard/vrbot/" + fileName;
+
+//            FileOutputStream os = new FileOutputStream(String.format("/sdcard/vrbot/%d.jpg", System.currentTimeMillis()));
+            FileOutputStream os = new FileOutputStream(fileName);
             os.write(data);
 //            vrSurfaceView.showMessage(os.getFD().toString());
             os.close();
